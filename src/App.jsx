28869bb8f3f57ed4aa99e45c556a49b048cf171b
Abdurehman Saeed <asaeed526@gmail.com>
2022-10-03 15:43:36 +0300

    edited resistor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+
+
 import './App.css';
 import FiveBandHome from './components/pages/fiveBand/FiveBandHome';
+import FiveBandResistor from './components/pages/fiveBand/FiveBandResistor';
 import FourBandHome from './components/pages/fourBand/FourBandHome';
 import SixBandHome from './components/pages/sixBand/SixBandHome';
 import FourBand from './images/fourBandManual.jpg'
+import FiveBand from './images/fiveBandManual.jpg'
+import sixBand from './images/sixBandManual.jpg'
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import { NavBar } from './components/NavBar';
 import Modal from './components/Modal';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LoadingPage from './components/LoadingPage';
 
 
@@ -25,7 +30,7 @@ function App() {
     {IsClosed?<Modal title={title} image={image} setClose={setClose}/>:''}
     <Router>
     
-       {IsPageLoading?<LoadingPage setLoading={setLoading}/>:''}
+       {/* {IsPageLoading?<LoadingPage setLoading={setLoading}/>:''} */}
          <NavBar setTitle={setTitle} setImage={setImage} setClose={setClose}/>
       <Routes>
         
